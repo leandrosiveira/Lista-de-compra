649787e93bd690e8eba724d4b8dd7c30b4afd933
leandrosiveira <leandrojazzmetal@gmail.com>
2024-12-03 11:54:59 -0300

    tentativa de JS falhada

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-const section = document.querySelector("section")
+// const section = document.querySelector("section")
 
-const newItem = document.querySelector("div.item")
+// const div = document.querySelector("div.item")
 
-const input = document.querySelector("input[type=checkbox][name=item]")
-const item = document.getElementById("add_item")
-const nameItem = document.querySelector("p")
-const deleteImg = document.querySelector('img[src="style/assets/icons/delete.png"][alt="Icone de delete"]')
+// const input = document.querySelector("input[type=checkbox][name=item]")
+// const item = document.getElementById("add_item")
+// const nameItem = document.querySelector("p")
+// const deleteImg = document.querySelector('img[src="style/assets/icons/delete.png"][alt="Icone de delete"]')
 
 // newItem.append(input, nameItem, deleteImg)
-// section.prepend(newItem)
+// section.prepend(div)
 
 // nameItem.textContent = "Saco de Lixo"
 // console.log(section.innerHTML)
 const button = document.querySelector("button")
+const form = document.querySelector("form")
 
-button.addEventListener("click", (event) => {
-  // event.preventDefault()
-  nameItem.textContent = item.value
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const newItem = {
+    newItem: input.value,
+  }
+  const section = document.querySelector("section")
   
-  newItem.append(input, nameItem, deleteImg)
-  section.prepend(newItem)
+  const div = document.querySelector("div.item")
+ 
+  
+  const input = document.querySelector("input[type=checkbox][name=item]")
+  const item = document.getElementById("add_item")
+  const nameItem = document.querySelector("p")
+  const deleteImg = document.querySelector('img[src="style/assets/icons/delete.png"][alt="Icone de delete"]')
+  
+  nameItem.textContent = this.newItem
+  div.append(input, nameItem, deleteImg)
+  section.prepend(div)
 })
