68f120b3337a5d502673fa8c7dc05a4c33594c85
leandrosiveira <leandrojazzmetal@gmail.com>
2024-12-03 18:24:49 -0300

    adicionar item em js funcionado

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-// const section = document.querySelector("section")
-
-// const div = document.querySelector("div.item")
-
-// const input = document.querySelector("input[type=checkbox][name=item]")
-// const item = document.getElementById("add_item")
-// const nameItem = document.querySelector("p")
-// const deleteImg = document.querySelector('img[src="style/assets/icons/delete.png"][alt="Icone de delete"]')
-
-// newItem.append(input, nameItem, deleteImg)
-// section.prepend(div)
-
-// nameItem.textContent = "Saco de Lixo"
-// console.log(section.innerHTML)
-const button = document.querySelector("button")
 const form = document.querySelector("form")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
 
-  const newItem = {
-    newItem: input.value,
-  }
   const section = document.querySelector("section")
   
-  const div = document.querySelector("div.item")
- 
+  const div = document.createElement("div")
+  div.classList.add("item")
   
-  const input = document.querySelector("input[type=checkbox][name=item]")
-  const item = document.getElementById("add_item")
-  const nameItem = document.querySelector("p")
-  const deleteImg = document.querySelector('img[src="style/assets/icons/delete.png"][alt="Icone de delete"]')
+  const input = document.createElement("input")
+  input.setAttribute('type', 'checkbox')
+  input.setAttribute('name', 'item')
   
-  nameItem.textContent = this.newItem
+  const nameItem = document.createElement("p")
+  const item = document.querySelector("input")
+  nameItem.textContent = item.value
+
+  
+  const deleteImg = document.createElement('img')
+  deleteImg.setAttribute('src', 'style/assets/icons/delete.png')
+  deleteImg.setAttribute('alt', 'Icone de delete')
+  
   div.append(input, nameItem, deleteImg)
   section.prepend(div)
+
+  const addItem = document.getElementById("add-item")
+  addItem.value = ""
 })
